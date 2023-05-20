0c4602807d7eea689f83a558acbf70f59ad082b1
Rafael dos Santos <rafaels1997@LIVE.COM>
2023-05-20 19:28:41 -0300

    Correção do layout da tela de cadastro e iniciadas validações

--- controller/usuarioController.js
+++ controller/usuarioController.js
@@ -1,17 +1,28 @@
-$(document).ready(function() {
-    $("#BtnCadastrar").click(function() {
-      let nome = $('#Nome').val();
-      let senha = $('#Senha').val();
-      let email = $('#Email').val();
-      let telefone = $('#Telefone').val();
-      let cpf = $('#Cpf').val();
-      let rg = $('#Rg').val();
+$(document).ready(function () {
+  $('#Nome').tooltip({ placement: 'bottom' });
 
-      let novoUsuario = new Usuario(nome, senha, email, telefone, cpf, rg);
-      console.log(novoUsuario);
-      novoUsuario.salvar();
-    });
+  $("#BtnCadastrar").click(function () {
+    let nome = $('#Nome').val();
+    let senha = $('#Senha').val();
+    let email = $('#Email').val();
+    let telefone = $('#Telefone').val();
+    let cpf = $('#Cpf').val();
+    let rg = $('#Rg').val();
+
+    validaDados(nome)
+
+    let novoUsuario = new Usuario(nome, senha, email, telefone, cpf, rg);
+    console.log(novoUsuario);
+    novoUsuario.salvar();
   });
+
+  function validaDados(nome) {
+    if (nome == "" || nome == null || typeof nome == 'undefined') {
+      $('#Nome').addClass('is-invalid');
+      return false;
+    }
+  }
+});
 
 /*id: PK int
 nome: string
