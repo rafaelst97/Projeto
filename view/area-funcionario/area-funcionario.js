e28d27f437bf535966b2717783bc60eb8a066878
Rafael dos Santos <rafaels1997@LIVE.COM>
2023-06-18 15:45:17 -0300

    Relatório de vendas finalizado

--- view/area-funcionario/area-funcionario.js
+++ view/area-funcionario/area-funcionario.js
@@ -6,25 +6,23 @@ $(document).ready(function () {
     const dbSize = 300 * 1024 * 1024; // 300mb
     const db = openDatabase(dbName, dbVersion, dbDisplayName, dbSize);
 
-    // this.trazerRelatorio = function () {
-        db.transaction(function (tx) {
-            tx.executeSql('SELECT * FROM Pedidos LEFT JOIN ItensPedido ON Pedidos.rowid = ItensPedido.idPedido LEFT JOIN ItensCardapio ON ItensCardapio.rowid = ItensPedido.idItem', [], function (tx, results) {
-                var len = results.rows.length, i;
-                debugger;
-                for (i = 0; i < len; i++) {
-                    var item = results.rows.item(i);
-                    $("#relatorio").append("<tr><td>" + item.id + "</td><td>" + item.data + "</td><td>" + item.valor + "</td><td>" + item.tipo + "</td></tr>");
-                }
-            }, null);
-        });
-    // }
+    db.transaction(function (tx) {
+        tx.executeSql('SELECT Pedidos.rowid AS idPedido, Usuarios.nome AS usuarioNome, (SELECT SUM(ItensCardapio.preco * ItensPedido.quantidade) FROM ItensPedido LEFT JOIN ItensCardapio ON ItensCardapio.rowid = ItensPedido.idItem WHERE ItensPedido.idPedido = Pedidos.rowid) AS valorTotal FROM Pedidos LEFT JOIN Usuarios ON Usuarios.rowid = Pedidos.idUsuario', [], function (tx, results) {
+            var len = results.rows.length, i;
+            console.log(results);
+            for (i = 0; i < len; i++) {
+                var item = results.rows.item(i);
+                $("#TablePedidosBody").append("<tr><td>" + item.idPedido + "</td><td>" + item.usuarioNome + "</td><td>R$" + item.valorTotal.toString().replace('.', ',') + "</td></tr>");
+            }
+        }, null);
+    });
 
     $("#BtnRelatorioVendas").click(function () {
         window.location.href = "relatorio-vendas.html";
     });
 
     $("#BtnRelatorioGorjetas").click(function () {
-        window.location.href = "/relatorio-gorjetas";
+        window.location.href = "relatorio-gorjetas.html";
     });
 
 });
